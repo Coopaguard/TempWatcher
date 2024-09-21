29d416876a8828f947ea21632a254f1bcc4eb999
Coopaguard <121747487+Coopaguard@users.noreply.github.com>
2024-09-21 14:41:22 +0200

    manage transluent (#5)

--- tempWatcher/Statics/Config.cs
+++ tempWatcher/Statics/Config.cs
@@ -59,6 +59,8 @@ namespace tempWatcher.Statics
 
         public bool AllayOnTop {  get; set; } = false;
 
+        public bool IsTransluent { get; set; } = false;
+
         public bool LockConfig { get; set; } = false;
 
         public Point? WindowSize {  get; set; }
